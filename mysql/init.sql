9fd29b6b30ba64e73189f8a7122bb914744f9488
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-11-04 16:24:56 -0600

    Create init.sql

---
+++ mysql/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE distancia (
+    clave INT PRIMARY KEY,
+    descripcion VARCHAR(50)
+);
+
+INSERT INTO distancia (clave, descripcion) VALUES
+(1, 'Descripción 1'),
+(2, 'Descripción 2');
