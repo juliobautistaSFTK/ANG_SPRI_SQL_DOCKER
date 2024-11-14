71e8df0beadbb9597ec285a2403bce1701f7bca6
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-11-14 09:51:49 -0600

    Create environment.ts

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',  // URL para las APIs en desarrollo
+  appVersion: '1.0.0',              // Versión de la aplicación (para desarrollo)
+};
