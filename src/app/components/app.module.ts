bb2fd914baded6b27cb19f5ade4a392f140b5235
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-11-14 09:49:55 -0600

    Create app.module.ts

---
+++ src/app/components/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
