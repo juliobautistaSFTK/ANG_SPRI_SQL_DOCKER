3b83e41180cd91f8eab92839732cef05df681f48
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-11-14 09:52:05 -0600

    Create environment.prod.ts

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.miapp.com',  // URL para las APIs en producción
+  appVersion: '1.0.0',              // Versión de la aplicación (para producción)
+};
