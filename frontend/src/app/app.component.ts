8018bba21cc2222688902d00d1ae24ac4ea037eb
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-11-15 15:53:24 -0600

    Update app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';  // Asegúrate de importar HttpErrorResponse
+
 
 @Component({
   selector: 'app-root',
