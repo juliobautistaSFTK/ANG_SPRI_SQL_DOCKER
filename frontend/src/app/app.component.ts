79c435bc0c7b9016788a8b9cd5987c8bc2280b92
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-11-17 09:44:59 -0600

    Update app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';  // Asegúrate de importar HttpErrorResponse
-import { HttpClientModule } from '@angular/common/http';
-
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -13,27 +8,5 @@ import { HttpClientModule } from '@angular/common/http';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  clave: string = '';
-  descripcion: string = '';
-
-  constructor(private http: HttpClient) { }
-
-  // Método para manejar el submit
-  onSubmit() {
-    if (this.clave) {
-      this.getDescripcion(this.clave); // Llama a la API si la clave está definida
-    }
-  }
-
-  // Método para obtener la descripción
-  getDescripcion(clave: string) {
-    this.http.get<any>(`http://localhost:8080/descripcion/${clave}`).subscribe(
-      response => {
-        this.descripcion = response.descripcion; // Asigna la respuesta al campo 'descripcion'
-      },
-      error => {
-        console.error('Error al obtener la descripción:', error);
-      }
-    );
-  }
+  title = 'frontend';
 }
