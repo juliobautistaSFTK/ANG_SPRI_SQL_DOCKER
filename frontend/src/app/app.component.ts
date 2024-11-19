2b580d704b4299b457e63c1979a83eb01010cfb1
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-11-19 14:51:06 -0600

    Add files via upload

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MyService } from './http.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  
+  public data: any;
+
+  constructor(
+    private readonly myService: MyService
+  ){}
+  
+  ngOnInit(): void {
+    this.myService.getData().then(data => {
+      this.data = data;
+    });
+  }
+
+}
