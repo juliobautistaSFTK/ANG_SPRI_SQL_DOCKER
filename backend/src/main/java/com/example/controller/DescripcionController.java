67d841867639514b5d04d0b75ffd6e5285ea6960
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-11-20 16:52:54 -0600

    Update DescripcionController.java

--- backend/src/main/java/com/example/controller/DescripcionController.java
+++ backend/src/main/java/com/example/controller/DescripcionController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:4200")
 public class DescripcionController {
 
     @Autowired
@@ -25,7 +27,7 @@ public class DescripcionController {
             statement.setInt(1, clave);
             ResultSet resultSet = statement.executeQuery();
             if (resultSet.next()) {
-                descripcion = resultSet.getString("descripcion");
+                descripcion = resultSet.getString("descripcion=");
             }
         } catch (Exception e) {
             e.printStackTrace();
