de08b474141e8edcfdf5d6eb20d803c060301b17
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2024-11-21 12:42:25 -0600

    Update DescripcionController.java

--- backend/src/main/java/com/example/controller/DescripcionController.java
+++ backend/src/main/java/com/example/controller/DescripcionController.java
@@ -12,7 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 @RestController
-@CrossOrigin(origins = "http://127.0.0.1:4200")
+@CrossOrigin(origins = "http://127.0.0.1:4201")
 public class DescripcionController {
 
     @Autowired
